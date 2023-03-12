1c5d923e3bab08559bac56a6a33b6fef57b21247
Mahavirsinh Chauhan <100768509+mahavir-amoga@users.noreply.github.com>
2023-03-12 23:03:50 +0530

    pdf download

--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Profile
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io
 
 # Create your views here.
 
@@ -24,3 +28,20 @@ def accept(request):
         profile.save()
 
     return render(request, 'pdf/accept.html')
+
+def resume(request, id):
+    user_profile = Profile.objects.get(pk=id)
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile': user_profile})
+    options = {
+        'page-size': 'Letter',
+        'encoding': 'UTF-8',
+    }
+    pdf = pdfkit.from_string(html, False, options)
+
+    response = HttpResponse(pdf, content_type='application/pdf')
+    filename = 'resume.pdf'
+    response['Content-Disposition'] = 'attachment'
+
+    return response
+    # return render(request, 'pdf/resume.html', {'user_profile': user_profile})
